d676ef0de330b69137edf1226d5576b8d744f347
Isaac Arnold <isaac@iaivphotography.com>
2020-09-23 18:16:34 -0500

    prompts in place and functioning (with placeholder results)

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//make array with symbols, one with numbersone with uppercase, one with lower case
-var symb [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}"]
-var num ["0123456789"]
-var lc ["abcdefghijklmnopqrstuvwxyz"]
-var uc ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+//make array with symbols, one with numbers, one with uppercase, one with lower case
+var charBank = []
+var symb = [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}"]
+var num = ["0123456789"]
+var lc = ["abcdefghijklmnopqrstuvwxyz"]
+var uc = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 
 //prompts for selection criteria
-var passwordLength= prompt("How many characters would you like your password to be?");
-var speChar= confirm("Would you like special characters in your password?");
-var lwr= confirm("Would you like lower case letters in your password?");
-var uppr= confirm("Would you like upper case letters in your password?");
-var nmbr= confirm("Would you like numbers in your password?")
+function generatePassword() {
+  var passwordLength= prompt("How many characters would you like your password to be?");
+    if (passwordLength > 7 && passwordLength < 126) {
+      alert ("test")
+    }
+    else {
+      alert ("please select a number from 8-125");
+    }
+  var speChar= confirm("Would you like special characters in your password?");
+    if (speChar === true) {
+      alert("test yes")
+    }
+    else {
+      alert ("test no");
+    }
+  var lwr= confirm("Would you like lower case letters in your password?");
+    if(lwr === true) {
+      alert("test yes")
+    }
+    else {
+      alert("test no)");
+    }
+  var upr= confirm("Would you like upper case letters in your password?");
+    if (upr === true) {
+      alert ("test yes");
+    }
+    else {
+      alert ("test no");
+    }
+  var nmbr= confirm("Would you like numbers in your password?")
+    if (nmbr === true) {
+      alert ("test yes");
+    }
+    else {
+      alert("test no");
+    }
+
+}
+
 
 
 
 //create number generator that selects randomly from 4 arrays based on selection criteria ???
 
 
-// Write password to the #password input
+// Write password to the #password input`s
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
