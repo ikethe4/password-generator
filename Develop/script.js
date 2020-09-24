8d9c4eaffc5564e1fc29692e7f094c85a0696981
Isaac Arnold <isaac@iaivphotography.com>
2020-09-23 21:58:14 -0500

    prompts work.  Arrays fill based on selecton.

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 //make array with symbols, one with numbers, one with uppercase, one with lower case
 var charBank = []
-var symb = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}"]
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// var symb = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}"]
+// var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+// var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// var uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 //prompts for selection criteria
 function generatePassword() {
-  var passwordLength= prompt("How many ch aracters would you like your password to be?");
-    if (passwordLength > 7 && passwordLength < 126) {
+  var passwordLength= prompt("How many characters would you like your password to be?");
+    if (passwordLength > 7 && passwordLength < 129) {
       alert ("test")
     }
     else {
@@ -18,31 +18,38 @@ function generatePassword() {
     }
   var speChar= confirm("Would you like special characters in your password?");
     if (speChar === true) {
-      alert("test yes")
+      alert("Password will contain special characters");
+      charBank.push(" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}");
+      console.log(charBank);
     }
     else {
-      alert ("test no");
+      alert ("Password will not contain special characters");
     }
   var lwr= confirm("Would you like lower case letters in your password?");
     if(lwr === true) {
-      alert("test yes")
+      alert("Password will contain lower case letters");
+      charBank.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+      console.log(charBank);
     }
     else {
-      alert("test no)");
+      alert("Password will not contail lower case letters");
     }
   var upr= confirm("Would you like upper case letters in your password?");
     if (upr === true) {
-      alert ("test yes");
+      alert ("Password will contain upper case letters");
+      charBank.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+      console.log(charBank);
     }
     else {
-      alert ("test no");
+      alert ("Password will not contain upper case letters");
     }
   var nmbr= confirm("Would you like numbers in your password?")
     if (nmbr === true) {
-      alert ("test yes");
+      alert ("Password will contain numbers");
+      charBank.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
     }
     else {
-      alert("test no");
+      alert("Password will not contain numbers");
     }
 
 }
